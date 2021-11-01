cd21e3f8accc81dc19e31a0871aeb9d5c640ca5e
Faizila Amlani <amlani.faizila@gmail.com>
2021-11-01 17:05:03 -0500

    added sign up form

--- controllers/api/commentRoutes.js
+++ controllers/api/commentRoutes.js
@@ -1,6 +1,6 @@
 // import 
 const router = require('express').Router();
-const { User, Post, Comment } = require('../models');
+const { User, Post, Comment } = require('../../models');
 const withAuth = require('../../utils/auth');
 
 // GET all
