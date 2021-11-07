c7c8049904cd49eaa75c65272a2543c6e6d47394
Faizila Amlani <amlani.faizila@gmail.com>
2021-11-07 14:03:50 -0600

    added time stamp on post and dashboard link on homepage

---
+++ controllers/dashboardRoutes.js
@@ -0,0 +1,36 @@
+// import
+const router = require('express').Router();
+const { User, Post, Comment } = require('../models');
+const withAuth = require('../utils/auth');
+
+// /dashboard
+
+//GET all post 
+router.post('/', withAuth, async (req, res) => {
+  try {
+    // Get all posts and JOIN with username
+    const postData = await Post.findAll({
+      include: [
+        {
+          model: User,
+          attributes: ['username'],
+        },
+      ],
+    });
+
+    // Serialize data so the template can read it
+    const posts = postData.map((post) => post.get({ plain: true }));
+
+    // dashboard.handlebars
+    res.render('homapage', { 
+      posts, 
+      logged_in: req.session.logged_in 
+    });
+    // error
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// export
+module.exports = router;
